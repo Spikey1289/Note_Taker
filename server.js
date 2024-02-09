f3f2bde3177d706310a87a2fbcf172bb43aa82e6
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-02-09 15:18:27 -0500

    MVP: added a post route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
+const fs = require('fs');
 const {v4: uuidv4} = require('uuid');
 
 const PORT = process.env.PORT || 3001;
@@ -15,27 +16,50 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//express midware to serve static pages
-app.use(express.static('public'));
+//POST route for new notes
+app.post('/api/notes', (req, res) => {
+    //generate new id
+    const newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    };
+    console.log(newNote);
+
+    //retrieve data from json file into array
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            //parsed notes array
+            const parsedNote = JSON.parse(data);
+
+            //push onto array
+            parsedNote.push(newNote);
+
+            //save as json file
+            fs.writeFile(
+                './db/db.json', 
+                JSON.stringify(parsedNote, null, 4),
+                (writeErr) => writeErr ? console.error(writeErr) : console.info('updated notes')
+            );
+        }
+    })
+
+    //send response
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+
+    res.json(response);
+});
 
 //retrieve all existing notes from server
 app.get('/api/notes', (req, res) => res.json(notes));
 
-//POST route for new notes
-app.post('/api/note', (req, res) => {
-    //retrieve data from json file into array
-
-    //generate new id
-
-    //create new object
-
-    //push onto array
-
-    //save as json file
-
-    //send response
-});
-
+//express midware to serve static pages
+app.use(express.static('public'));
 
 //if route dne, serve homepage
 app.get('*', (req, res) => {
